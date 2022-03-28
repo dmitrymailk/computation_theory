7609af9908ff97639735077b9d236495f35f6dc7
dmitrymailk <dmitrymail_k@mail.ru>
2022-03-28 18:56:58 +0300

    add cos to RPM

--- lab_2/reverse_polish_form.py
+++ lab_2/reverse_polish_form.py
@@ -1,4 +1,4 @@
-from signal import signal
+import math
 
 
 infix_notation = "3+3*3-3"
@@ -105,7 +105,8 @@ def infix_to_postfix_3(infix_notation):
             num = f"{char}"
             if i < len(infix_notation) - 1:
                 while (
-                    infix_notation[i + 1].isdigit() or infix_notation[i + 1] == "."
+                    infix_notation[i +
+                                   1].isdigit() or infix_notation[i + 1] == "."
                 ) and i < len(infix_notation):
                     num += infix_notation[i + 1]
                     i += 1
@@ -180,7 +181,8 @@ def infix_to_postfix_4(infix_notation, delimetr=""):
             num = f"{char}"
             if i < len(infix_notation) - 1:
                 while (
-                    infix_notation[i + 1].isdigit() or infix_notation[i + 1] == "."
+                    infix_notation[i +
+                                   1].isdigit() or infix_notation[i + 1] == "."
                 ) and i < len(infix_notation):
                     num += infix_notation[i + 1]
                     i += 1
@@ -241,6 +243,97 @@ def infix_to_postfix_4(infix_notation, delimetr=""):
                     stack.append(char)
                 i += 1
 
+
+def infix_to_postfix_5(infix_notation, delimetr=""):
+    """
+    convertion for multiple digits
+    with parentheses, unary minus and cos
+    """
+    stack = []
+    RPM = []
+
+    strength = {
+        "+": 1,
+        "-": 1,
+        "*": 2,
+        "/": 2,
+        "cos": 2
+    }
+
+    i = 0
+    while i < len(infix_notation):
+        char: str = infix_notation[i]
+        if char.isdigit():
+            num = f"{char}"
+            if i < len(infix_notation) - 1:
+                while (
+                    infix_notation[i +
+                                   1].isdigit() or infix_notation[i + 1] == "."
+                ) and i < len(infix_notation):
+                    num += infix_notation[i + 1]
+                    i += 1
+
+            i += 1
+            num += delimetr
+            RPM.append(num)
+            if i > 1:
+                if infix_notation[i - 1] == "-" and infix_notation[i - 2] == "(":
+                    RPM.append("-")
+
+        elif char in "-+*/()":
+            if len(stack) > 0:
+                if not char in "()":
+
+                    last_char = stack[-1]
+                    if not last_char in ["(", ")"]:
+                        if strength[char] > strength[last_char]:
+                            stack.append(char)
+                            i += 1
+                        else:
+                            while (
+                                len(stack) > 0 and strength[char] <= strength[last_char]
+                            ):
+                                if stack[-1] == "(":
+                                    break
+                                last_char = stack.pop()
+                                RPM.append(last_char)
+                            stack.append(char)
+                            i += 1
+                    else:
+                        if (
+                            infix_notation[i - 1] == "("
+                            and char == "-"
+                        ):
+                            RPM.append("0" + delimetr)
+                            stack.append("-")
+                        #     RPM.append("-")
+                        else:
+                            stack.append(char)
+                        i += 1
+                else:
+                    if char == "(":
+                        stack.append(char)
+                        i += 1
+                    else:
+                        last_char = stack.pop()
+                        while len(stack) > 0 and last_char != "(":
+                            RPM.append(last_char)
+                            last_char = stack.pop()
+                        i += 1
+            else:
+                if char == "-":
+                    RPM.append("0" + delimetr)
+                    stack.append("-")
+                else:
+                    stack.append(char)
+                i += 1
+        elif char.isalpha():
+            if infix_notation[i:i+3] == 'cos':
+                print("cos")
+                RPM.append("0")
+                stack.append('cos')
+                i += 3
+
     # simply getting operations from stack and passing them to RPM
     for char in stack[::-1]:
         RPM.append(char)
@@ -263,6 +356,8 @@ def postfix_calculation(RPM):
             result = num_1 / num_2
         elif sign == "*":
             result = num_1 * num_2
+        elif sign == 'cos':
+            result = math.cos(num_2)
 
         return result
 
@@ -286,14 +381,16 @@ def postfix_calculation(RPM):
 
 # examples = [["5+(6.6+9-5.2)/(0.8+1*2)+7", "0;5;-+6.6;9;+5.2;0.8;1;2;*+/7;+"]]
 # examples = [["-55+(-60.70-9)*(-7)-(120+3*4.56)/(-22)", "569-7*+"]]
-examples = [["(-3.5+2*2.25)*7+1", "569-7*+"]]
+# examples = [["-(-3.5+2*2.25)*7+1*cos(1+5*4)", "569-7*+"]]
+examples = ['-cos(2*3+2)*cos(2+3*5)-5*6']
 # 05-+69+58;1;2;*+/7;++
 for item in examples:
-    RPM = infix_to_postfix_4(item[0], delimetr="")
+    RPM = infix_to_postfix_5(item, delimetr="")
     # if predict != item[1]:
     print(RPM)
     result = postfix_calculation(RPM)
-    print(result, eval(item[0]))
+    eval_result = "exec('from math import *') or "+item
+    print(result, eval(eval_result))
     # print(f"{item[1]}\n{predict}")
 
     # добавить корень, синусы, косинусы, степени
