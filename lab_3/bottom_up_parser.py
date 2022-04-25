c2e236efa21e724ef7584fb4976bce28263b673d
dmitrymailk <dmitrymail_k@mail.ru>
2022-04-25 23:38:18 +0300

    initial bottom up parser

--- lab_3/bottom_up_parser.py
+++ lab_3/bottom_up_parser.py
@@ -19,7 +19,7 @@ Table = {
     '(': ["s8", "", "s8", "", "", "s8", "", "", "s8", "", "", ""],
     ')': ["", "s11", "", "r0", "r1", "", "r2", "r3", "", "s3", "r4", "r5"],
     'x': ["s11", "", "s11", "", "", "s11", "", "", "s11", "", "", ""],
-    ';': ["", "acc", "", "r0", "r1", "", "r2", "r3", "", "", "r4", "r5"],
+    '@': ["", "end", "", "r0", "r1", "", "r2", "r3", "", "", "r4", "r5"],
 }
 
 initial_string = "int+(int+int+int)"
@@ -29,11 +29,6 @@ class BottomUpParser:
     def __init__(self, expression):
         # @ is a end of string
         self.expression = f"{expression}@"
-
-    def LR(self):
-        stateStack = deque([])
-        readyForm = ""
-        stateStack.append(0)
 
     def get_action_name(self, state, symbol):
         l = Table.get(symbol, None)
@@ -50,7 +45,7 @@ class BottomUpParser:
         elif 'r' in action:
             return 'reduce'
         else:
-            "error"
+            return "error"
 
     def get_action_number(self, state, symbol):
         l = Table.get(symbol, None)
@@ -59,19 +54,65 @@ class BottomUpParser:
 
         action = l[state]
 
-        return int(action[1])
+        return int(action[1:])
 
     def get_replace(self, string):
         add = ""
-        while (len(string) != 0):
+        string = string[:]
+        while len(string) != 0:
             for item in Rule.keys():
                 if Rule[item] == string:
-                    return add + item
+                    return add + item[0]
 
             add += string[0]
             string = string[1:]
 
         return "Replace not found"
+
+    def format_state_stack(self, stk):
+        result = ""
+        states = stk[:]
+
+        while len(states) != 0:
+            result += str(states.pop(0)) + " "
+
+        return result
+
+    def LR(self, ):
+        stateStack = []
+        readyForm = ""
+
+        # Подготовка к первому шагу
+        stateStack.append(0)
+        token = self.expression[0]
+
+        print("{0,20}  |  {1, 20}  |", "State stack", "Result")
+
+        while True:
+            state = stateStack[-1]
+            doing = self.get_action_name(state, token)
+
+            tmp = self.format_state_stack(stateStack)
+
+            tmp2 = readyForm + self.expression
+
+            print("{0,20}  |  {1, 20}  |", tmp, tmp2)
+
+            if doing == "end":
+                break
+
+            actNum = self.get_action_number(state, token)
+            stateStack.pop()
+
+            if doing == "shift":
+                stateStack.append(actNum)
+                readyForm += token
+                self.expression = self.expression[1:]
+                token = self.expression[0]
+            elif doing == "reduce":
+                readyForm = self.get_replace(readyForm)
+                actNum = self.get_action_number(actNum, readyForm[-1])
+                stateStack.append(actNum)
 
 
 parser = BottomUpParser("x+x*x")
