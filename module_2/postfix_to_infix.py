f28dda914eeec0c45f243cff1d848556379c0e88
dmitrymailk <dmitrymail_k@mail.ru>
2022-04-25 22:58:20 +0300

    fix ugly insert

--- module_2/postfix_to_infix.py
+++ module_2/postfix_to_infix.py
@@ -17,17 +17,17 @@ def postfix_to_infix(expression):
         else:
             operator_1 = stack.pop(0)
             operator_2 = stack.pop(0)
-            stack.insert(0, "(" + operator_2 + char +
-                         operator_1 + ")")
+            stack.insert(0, f"({operator_2}{char}{operator_1})")
             i += 1
 
     return stack[0]
 
 
 exp = "02232*+*2*-"
-exp = "0003-22*+sqrt7*-1154*+*356*+2+*0145*+1+sqrt*+"
 # sqrt(-3+2*2)*7+1*(1+5*4)*(3+5*6+2)*sqrt(1+4*5+1)
 exp = "003-22*+sqrt7*1154*+*356*+2+*0145*+1+sqrt*+"
 # 3+sqrt(5)*6/6+sqrt(7)
 exp = "305sqrt6*6/+07sqrt+"
+# ((0-(sqrt(((0-3)+(2*2)))*7))+(((1*(1+(5*4)))*((3+(5*6))+2))*sqrt(((1+(4*5))+1))))
+exp = "0003-22*+sqrt7*-1154*+*356*+2+*0145*+1+sqrt*+"
 print(postfix_to_infix(exp))
